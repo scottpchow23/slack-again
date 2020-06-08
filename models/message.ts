473f9ae91115c3a3a9cadb1d45b84cac3fd9e039
Scott Chow <tehchowster@gmail.com>
2020-06-07 21:41:25 -0700

    sc - correctly annotated nullables on Message type

--- models/message.ts
+++ models/message.ts
@@ -1,13 +1,14 @@
 export interface Message {
-  client_msg_id: string;
+  client_msg_id?: string;
   type: string;
+  subtype?: string;
   text: string;
   user: string;
   ts: string;
-  team: string;
-  user_team: string;
-  source_team: string;
-  user_profile: {
+  team?: string;
+  user_team?: string;
+  source_team?: string;
+  user_profile?: {
     avatar_hash: string;
     image_72: string;
     first_name: string;
@@ -18,7 +19,7 @@ export interface Message {
     is_restricted: boolean;
     is_ultra_restricted: boolean;
   };
-  blocks: {
+  blocks?: {
     type: string;
     block_id: string;
     elements: {
