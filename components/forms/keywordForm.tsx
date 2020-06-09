1c8b2a6823320d33b16d54dc0648030522fd0571
Scott Chow <tehchowster@gmail.com>
2020-06-08 20:43:10 -0700

    sc - add placeholder text to search input

--- components/forms/keywordForm.tsx
+++ components/forms/keywordForm.tsx
@@ -1,8 +1,12 @@
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import { FormEvent } from "react";
 
-export default function (props: { handleSubmit; setSearchTerm }) {
+export default function (props: {
+  handleSubmit: (e: FormEvent) => void;
+  setSearchTerm: (value: string) => void;
+}) {
   return (
     <Form onSubmit={props.handleSubmit}>
       <Form.Row className="Row">
@@ -10,11 +14,12 @@ export default function (props: { handleSubmit; setSearchTerm }) {
           <Form.Control
             type="text"
             name="searchTerm"
+            placeholder="Enter keyword here"
             onChange={(e) => props.setSearchTerm(e.target.value)}
           ></Form.Control>
         </Col>
         <Col xs="auto">
-          <Button type="submit" variant="primary" className="mb-2">
+          <Button type="submit" variant="primary">
             Search
           </Button>
         </Col>
